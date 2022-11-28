f2fb6120907806add570a12c26466a2ff3aa264b
IvanRomero03 <i.wells.ar@gmail.com>
2022-11-28 15:53:52 -0600

    empty

--- celdas.js
+++ celdas.js
@@ -1,7 +1,8 @@
 "use strict";
 const celdas = (io) => {
-    io.on("connection", ( /* socket */) => {
-        console.log("a user connected to celdas");
-    });
+  io.on("connection", (/* socket */) => {
+    console.log("a user connected to celdas");
+  });
 };
+
 module.exports = celdas;
