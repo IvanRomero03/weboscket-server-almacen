ec65fd661d488999821856471a68b386aad6707d
IvanRomero03 <i.wells.ar@gmail.com>
2022-11-30 05:06:16 -0600

    prueba

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const createServer = require("http").createServer;
 const Server = require("socket.io");
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const client = axios.create({
+    //baseURL: "http://localhost:3001/api",
+    baseURL: "http://almacen-iot.vercel.app/api",
+});
 const server = createServer(app);
 app.get("/health", (req, res) => {
     console.log("health check");
@@ -13,28 +18,62 @@ const io = new Server(server, {
         origin: "*",
     },
 });
-const celdasCallback = require("./celdas");
-const autorizacionCallback = require("./autorizacion");
-// enable CORS
-// io.on("connection", () => {
-//   console.log("a user connected");
-// });
-// receive message from "mensaje" channel
+let currentPetition = null;
 io.on("connection", (socket) => {
     console.log("a user connected");
+    socket.broadcast.emit("mensaje", "bienvenido!");
     socket.on("mensaje", (msg) => {
         console.log("message: " + msg);
         socket.broadcast.emit("mensaje", msg);
+    });
+    socket.on("createPetition", (peticion) => {
+        currentPetition = peticion;
+        socket.broadcast.emit("Peticion", peticion.credencial.credential);
+    });
+    socket.on("autorized", (msg) => {
+        console.log("autorized: " + msg);
+        for (let i = 0; i < currentPetition.celdas.length; i++) {
+            socket.broadcast.emit("open", currentPetition.celdas[i].celdaId);
+        }
+        socket.close();
+        client.post("/createPrestamo", currentPetition.prestamoInfo);
+        currentPetition = null;
+    });
+    socket.on("denied", (msg) => {
+        console.log("denied: " + msg);
+        currentPetition = null;
+    });
+    socket.on("timeout", (msg) => {
+        console.log("timeout: " + msg);
+        currentPetition = null;
     });
 });
 io.of("/celdas").on("connection", ( /* socket */) => {
     console.log("a user connected to celdas");
 });
-celdasCallback(io.of("/celdas"));
-io.of("/autorizacion").on("connection", ( /* socket */) => {
+io.of("/autorizacion").on("connection", (socket) => {
     console.log("a user connected to autorizacion");
+    socket.broadcast.emit("mensaje", "bienvenido a autorizacion");
+    socket.on("autorized", (msg) => {
+        console.log("autorized: " + msg);
+        io.of("/celdas").on("connection", (socketCeldas) => {
+            for (let i = 0; i < currentPetition.celdas.length; i++) {
+                socketCeldas.emit("open", currentPetition.celdas[i].celdaId);
+            }
+            socketCeldas.close();
+            client.post("/createPrestamo", currentPetition.prestamoInfo);
+            currentPetition = null;
+        });
+    });
+    socket.on("denied", (msg) => {
+        console.log("denied: " + msg);
+        currentPetition = null;
+    });
+    socket.on("timeout", (msg) => {
+        console.log("timeout: " + msg);
+        currentPetition = null;
+    });
 });
-autorizacionCallback(io.of("/autorizacion"));
 server.listen(process.env.PORT || 3000, () => {
     console.log("listening on *:3000");
 });
--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ const createServer = require("http").createServer;
 const Server = require("socket.io");
 const express = require("express");
 const app = express();
+const axios = require("axios");
+
+const client = axios.create({
+  //baseURL: "http://localhost:3001/api",
+  baseURL: "http://almacen-iot.vercel.app/api",
+});
 
 const server = createServer(app);
 
@@ -16,31 +22,87 @@ const io = new Server(server, {
   },
 });
 
-const celdasCallback = require("./celdas");
-const autorizacionCallback = require("./autorizacion");
+type Celda = {
+  celdaId: Number;
+  existencia: Number;
+};
 
-// enable CORS
+type credencial = {
+  credential: String;
+};
 
-// receive message from "mensaje" channel
+type prestamoInfo = {
+  userId: Number;
+  itemId: Number;
+  quantity: Number;
+};
+type Petition = {
+  celdas: Celda[];
+  credencial: credencial;
+  prestamoInfo: prestamoInfo;
+};
+
+let currentPetition: Petition = null as any;
+
 io.on("connection", (socket: any) => {
   console.log("a user connected");
+  socket.broadcast.emit("mensaje", "bienvenido!");
   socket.on("mensaje", (msg: any) => {
     console.log("message: " + msg);
 
     socket.broadcast.emit("mensaje", msg);
+  });
+
+  socket.on("createPetition", (peticion: Petition) => {
+    currentPetition = peticion;
+    socket.broadcast.emit("Peticion", peticion.credencial.credential);
+  });
+  socket.on("autorized", (msg: any) => {
+    console.log("autorized: " + msg);
+    for (let i = 0; i < currentPetition.celdas.length; i++) {
+      socket.broadcast.emit("open", currentPetition.celdas[i].celdaId);
+    }
+    socket.close();
+    client.post("/createPrestamo", currentPetition.prestamoInfo);
+    currentPetition = null as any;
+  });
+  socket.on("denied", (msg: any) => {
+    console.log("denied: " + msg);
+    currentPetition = null as any;
+  });
+  socket.on("timeout", (msg: any) => {
+    console.log("timeout: " + msg);
+    currentPetition = null as any;
   });
 });
 
 io.of("/celdas").on("connection", (/* socket */) => {
   console.log("a user connected to celdas");
 });
-//celdasCallback(io.of("/celdas"));
 
-io.of("/autorizacion").on("connection", (/* socket */) => {
+io.of("/autorizacion").on("connection", (socket: any) => {
   console.log("a user connected to autorizacion");
+  socket.broadcast.emit("mensaje", "bienvenido a autorizacion");
+  socket.on("autorized", (msg: any) => {
+    console.log("autorized: " + msg);
+    io.of("/celdas").on("connection", (socketCeldas: any) => {
+      for (let i = 0; i < currentPetition.celdas.length; i++) {
+        socketCeldas.emit("open", currentPetition.celdas[i].celdaId);
+      }
+      socketCeldas.close();
+      client.post("/createPrestamo", currentPetition.prestamoInfo);
+      currentPetition = null as any;
+    });
+  });
+  socket.on("denied", (msg: any) => {
+    console.log("denied: " + msg);
+    currentPetition = null as any;
+  });
+  socket.on("timeout", (msg: any) => {
+    console.log("timeout: " + msg);
+    currentPetition = null as any;
+  });
 });
-
-//autorizacionCallback(io.of("/autorizacion"));
 
 server.listen(process.env.PORT || 3000, () => {
   console.log("listening on *:3000");
